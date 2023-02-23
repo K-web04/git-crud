887eb3e462e903a8babe9bf5d8e73d1ccdfcb67f
Kumar Kriti <kumarkriti01@gmail.com>
2023-02-23 20:08:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", true);
+
+require("./db/conn");
+const users = require("./models/userSchema");
+const cors = require("cors");
+const router = require("./routes/router");
+
+const port = process.env.PORT || 8003;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("server start");
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`server is listening port number ${port}`);
+});
